fdf1efdcfa33e29143975a1911c62e39fb0fe562
nim-ka <alexing105@gmail.com>
2023-04-17 00:29:56 -0400

    I apologize for inflicting this code upon the world

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-import { getResistorOhms } from './resistor.js';
+import { getResistorOhms } from './resistor.js'
+
+document.querySelectorAll(".box").forEach((el) => el.addEventListener("click", (evt, bands = document.querySelectorAll("[id^=b]")) => (bands[evt.target.parentElement.id[5]].className = "band " + evt.target.textContent, document.querySelector(".answer").textContent = getResistorOhms(...[...bands].map((el) => el.className.split(" ")[1])))))
--- resistor.js
+++ resistor.js
@@ -9,6 +9,50 @@
    http://bit.ly/2NjS274
 */
 
-const getResistorOhms = (bands) => {};
+function decodeColor(color) {
+  return {
+    silver: -2,
+    gold: -1,
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9,
+  }[color]
+}
 
-export { getResistorOhms };
+function decodeTolerance(color) {
+  return {
+    brown: 1,
+    red: 2,
+    green: 0.5,
+    blue: 0.25,
+    violet: 0.1,
+    grey: 0.05,
+    gold: 5,
+    silver: 10,
+  }[color]
+}
+
+const getResistorOhms = (color1, color2, multiplier, tolerance) => {
+  let value = (decodeColor(color1) * 10 + decodeColor(color2)) * (10 ** decodeColor(multiplier))
+  const toleranceValue = decodeTolerance(tolerance)
+
+  let i = -1
+
+  while (value >= 1000 && i < 2) {
+    value /= 1000
+    i++
+  }
+
+  const valueStr = value + ("kMG"[i] ?? "")
+
+  return `Resistor value: ${valueStr} Ohms ±${toleranceValue}%`
+}
+
+export { getResistorOhms }
